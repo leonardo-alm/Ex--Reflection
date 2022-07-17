091cfbfa8e8d0107f10d84c37643b7ab217daff2
Leonardo Almeida <leonardo.olv94@gmail.com>
2022-07-17 13:47:11 -0300

    first commit

---
+++ Reflection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+
+namespace Reflection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            void DisplayPublicProperties()
+            {
+                PropertyInfo[] properties = typeof(Student).GetProperties();
+
+                foreach (PropertyInfo p in properties)
+                {
+                    Console.WriteLine(p.Name);                   
+                }
+            }
+            DisplayPublicProperties();
+        }
+    }
+}
